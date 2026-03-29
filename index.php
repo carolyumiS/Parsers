25b05a1b2d902d5649968ec1408224324afded6a
Carolina Yumi <153402855+carolyumiS@users.noreply.github.com>
2026-03-29 01:12:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
                     <li class="nav-item"><a class="nav-link" href="#servicos">Serviços</a></li>
                     <li class="nav-item"><a class="nav-link" href="#equipe">Equipe</a></li>
                     <li class="nav-item"><a class="nav-link" href="#swot">SWOT</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#contato">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#contato">Contato</a></li>
                     <li class="nav-item ms-lg-3 mt-3 mt-lg-0">
                         <a class="btn btn-nav" href="#contato">Fale Conosco</a>
                     </li>
